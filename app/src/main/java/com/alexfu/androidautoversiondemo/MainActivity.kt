dfad4f33335007f296201266bd37796c3bf244ce
Alex Fu <alex.fu914@gmail.com>
2020-02-22 09:01:05 -0500

    Initial commit.

---
+++ app/src/main/java/com/alexfu/androidautoversiondemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alexfu.androidautoversiondemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
